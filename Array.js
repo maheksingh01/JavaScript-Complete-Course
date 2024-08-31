ec761309b624902df20134e58db60fdaed3a0b8a
Mahek Singh <mahek.2002.singh@gmail.com>
2024-09-01 03:26:06 +0530

    Array In JS

---
+++ Array.js
@@ -0,0 +1,3 @@
+var names = ["Mahek", "Ankur", "Shivaay", "Rajesh", "Dinesh"];
+// console.log(names.length );    // to display number of count of items in array
+console.log(names[0]); // to display specific item from array
