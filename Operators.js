f430cf4c0f404cefb2a7d52e28304607a5ac1961
Mahek Singh <mahek.2002.singh@gmail.com>
2024-09-07 09:03:29 +0530

    operators in js

---
+++ Operators.js
@@ -0,0 +1,17 @@
+console.log("Operators In JS");
+
+let a = 45;
+let b = 18;
+let c = 2;
+let d = 3;
+
+// Arithmatic Operators
+console.log("Addition (a + b) : ", a + b);
+console.log("Substraction (a - b) : ", a - b);
+console.log("Multiplication (a * b) : ", a * b);
+console.log("Division (a / b) : ", a / b);
+console.log("Power (c ** d) : ", c ** d);
+console.log(++a);
+console.log(b++);
+console.log(a--);
+console.log(--b);
