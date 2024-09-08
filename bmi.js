6fb8b76acff7192c45ed17f7bab0d459f6d6ccb3
Mahek Singh <mahek.2002.singh@gmail.com>
2024-09-08 09:20:18 +0530

    updated bmi in js

--- bmi.js
+++ bmi.js
@@ -3,6 +3,6 @@ const prompt = require("prompt-sync")();
 var weight = prompt("Enter your weight : (in kgs) ");
 var height = prompt("Enter your height : (in meters) ");
 
-var bmi = weight / (height * height);
+var bmi = (weight / (height * height)) * 100;
 
 console.log(bmi);
