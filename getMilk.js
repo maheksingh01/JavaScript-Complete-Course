1029ceace79b70d8fb374b4d28b734e0c3a0ee45
Mahek Singh <mahek.2002.singh@gmail.com>
2024-09-17 11:48:48 +0530

    adding Get Milk

---
+++ getMilk.js
@@ -0,0 +1,24 @@
+function getMilk() {
+  console.log("Exit The House");
+  console.log("Move Forword");
+  console.log("Move Forword");
+  console.log("Move Up");
+  console.log("Move Up");
+  console.log("Move Up");
+  console.log("Move Up");
+  console.log("Move Right");
+  console.log("Move Right");
+  console.log("Enter The Shop");
+  console.log("Buy Milk");
+  console.log("Move Left");
+  console.log("Move Left");
+  console.log("Move Down");
+  console.log("Move Down");
+  console.log("Move Down");
+  console.log("Move Down");
+  console.log("Move Forword");
+  console.log("Move Forword");
+  console.log("Enter The House");
+}
+
+getMilk();
