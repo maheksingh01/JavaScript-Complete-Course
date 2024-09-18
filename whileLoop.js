4ad543a121756ea64baa66ce30b0bbabc06065ca
Mahek Singh <mahek.2002.singh@gmail.com>
2024-09-18 09:36:46 +0530

    adding: while loop

---
+++ whileLoop.js
@@ -0,0 +1,22 @@
+// while
+
+// const prompt = require("prompt-sync")();
+// let n = prompt("Enter the value of n : ");
+// n = Number.parseInt(n);
+// let i = 0;
+
+// while (i < n) {
+//   console.log(i);
+//   i++;
+// }
+
+// do-while
+const prompt = require("prompt-sync")();
+let n = prompt("Enter the value of n : ");
+n = Number.parseInt(n);
+let i = 0;
+
+do {
+  console.log(i);
+  i++;
+} while (i < n);
