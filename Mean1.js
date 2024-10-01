813bc404114b976f249145cc0b305450dc0ddeae
Mahek Singh <mahek.2002.singh@gmail.com>
2024-10-01 08:09:17 +0530

    adding: Mean

---
+++ Mean1.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")();
+
+n = prompt("Enter the total number of elements : ");
+num = [];
+
+for (let i = 0; i < n; i++) {
+  num = num.push(new_element);
+  return num;
+}
